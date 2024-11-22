0c955d8028eb61704dc8d297e3feeff9f5901820
hirohinata <azuri_hirohinata@hotmail.co.jp>
2024-11-23 02:05:25 +0900

    plus関数テスト

--- asmTest/main.asm
+++ asmTest/main.asm
@@ -1,7 +1,19 @@
 _text SEGMENT
+
+plus:
+        add rsi, rdi
+        mov rax, rsi
+        ret
+
+main:
+        mov rdi, 3
+        mov rsi, 4
+        call plus
+        ret
+
 mainCRTStartup PROC
 
-mov rax, 42
+call main
 ret
 
 mainCRTSTartup ENDP
