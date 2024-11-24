02b67ab052ebf84b42038a48f111ccf1d0f8d8b1
hirohinata <azuri_hirohinata@hotmail.co.jp>
2024-11-24 21:52:26 +0900

    値をそのまま返す処理の実装とテストの追加

---
+++ TestProject/Test1.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private static void CallCompiler(string artuments, out string? asm)
+        {
+            ProcessStartInfo psInfo = new()
+            {
+                FileName = "../../../../x64/Debug/chibicc.exe",
+                Arguments = artuments,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+
+            asm = Process.Start(psInfo)?.StandardOutput.ReadToEnd();
+        }
+
+        private static void CallGcc(string? asm, out string exeFileName)
+        {
+            var tempPath = $"{Path.GetTempPath()}chibicc";
+            Directory.CreateDirectory(tempPath);
+
+            var asmFileName = Path.Combine(tempPath, "test.s");
+            exeFileName = Path.Combine(tempPath, "test.exe");
+            File.WriteAllText(asmFileName, asm);
+
+            ProcessStartInfo psInfo = new()
+            {
+                FileName = "cmd",
+                Arguments = $"/c \"gcc {asmFileName} -o {exeFileName}\"",
+                WorkingDirectory = tempPath,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+
+            Process.Start(psInfo)?.WaitForExit();
+        }
+
+        private static void CallExe(string exeFileName, out int exitCode)
+        {
+            ProcessStartInfo psInfo = new()
+            {
+                FileName = exeFileName,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            var p = Process.Start(psInfo);
+            p?.WaitForExit();
+            exitCode = p?.ExitCode ?? -1;
+        }
+
+        private static int Compile(string args)
+        {
+            CallCompiler(args, out var asm);
+            CallGcc(asm, out var exeFileName);
+            CallExe(exeFileName, out var exitCode);
+            return exitCode;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, Compile("0"));
+            Assert.AreEqual(42, Compile("42"));
+        }
+    }
+}
